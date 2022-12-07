3e6a9d59c18582831a43a9c06a549f09b386e874
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-12-07 11:19:13 -0300

    feat: pokemon listing component

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,12 @@
 import * as trpcNext from '@trpc/server/adapters/next'
-import { appRouter } from '@/server/routers/_app'
+import { AppRouter, appRouter } from '@/server/routers/_app'
+import { inferProcedureOutput } from '@trpc/server'
 
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => ({}),
 })
+
+export type inferQueryResponse<
+  TRouteKey extends keyof AppRouter['_def']['procedures'],
+> = inferProcedureOutput<AppRouter['_def']['procedures'][TRouteKey]>
