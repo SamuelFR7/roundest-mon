bff6cd57f677d250ffb8754a473ad766eb42081b
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-12-07 09:42:59 -0300

    feat: get random pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,16 @@
-import { trpc } from '@/utils/trpc'
+import { getOptionsForVote } from '@/utils/getRandomPokemon'
 
 export default function Home() {
-  const { data, isLoading } = trpc.hello.useQuery({
-    text: 'Samuel',
-  })
-
-  if (isLoading) {
-    return <div>Is loading</div>
-  }
-
-  if (data) {
-    return <div>{data.greeting}</div>
-  }
+  const [first, second] = getOptionsForVote()
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   )
