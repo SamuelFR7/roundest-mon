da07d09aabb26e8fd113561a31baa746d10072e2
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-12-07 09:07:33 -0300

    feat: with trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,18 @@
+import { trpc } from '@/utils/trpc'
+
 export default function Home() {
+  const { data, isLoading } = trpc.hello.useQuery({
+    text: 'Samuel',
+  })
+
+  if (isLoading) {
+    return <div>Is loading</div>
+  }
+
+  if (data) {
+    return <div>{data.greeting}</div>
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which pokemon is rounder?</div>
