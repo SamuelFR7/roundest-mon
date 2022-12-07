9ec3d8cc4cea23f5b2689c738f68b7f0a1665f5e
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-12-07 08:50:56 -0300

    feat: basic ui structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Hello world</div>
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which pokemon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  )
 }
